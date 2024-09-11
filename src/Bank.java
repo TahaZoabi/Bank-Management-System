5667d3452a47741d349c5039c8eabc18af5f5cb9
Taha Zoabi <zoabi03@gmail.com>
2024-09-11 11:08:24 +0300

    fixed list

--- src/Bank.java
+++ src/Bank.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
-    private String name;
-    private List<Client> customerList;
+    private final String name;
+    private List<Account> accountsList;
 
 
     public Account createAccount(AccountType type) {
@@ -12,19 +12,27 @@ public class Bank {
         }
         return new CheckingAccount(500);
     }
-    
+
+    public void addAccount(Account account) {
+        accountsList.add(account);
+    }
+
+    public void removeAccount(Account account) {
+        accountsList.remove(account);
+    }
+
 
     public Bank(String name) {
         this.name = name;
-        customerList = new ArrayList<>();
+        accountsList = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
 
-    public List<Client> getCustomerList() {
-        return customerList;
+    public List<Account> getAccountsList() {
+        return accountsList;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
         Bank bank = new Bank("Vertex Bank");
         Account accountOne = bank.createAccount(AccountType.SAVINGS_ACCOUNT);
         Account accountTwo = bank.createAccount(AccountType.CHECKING_ACCOUNT);
+        bank.addAccount(accountOne);
+        bank.addAccount(accountTwo);
+
+        Client client = new Client("Taha Zoabi", 1);
+
+        System.out.println(bank.getAccountsList());
 
         System.out.println(accountOne.getBalance());
         accountOne.depositBalance(15);
